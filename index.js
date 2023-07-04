d0b6a0fcda862090696ffea0e076a72ea78e7ace
ArturoDuCas <a00833516@tec.mx>
2023-07-03 19:32:51 -0600

    feat: Implement /api/whoami endpoint

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// index.js
-// where your node app starts
-
-// init project
+// Init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
@@ -9,7 +6,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,10 +16,20 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/api/whoami", (req, res) => {
+  console.log(req);
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+
+  })
+})
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
